556bb39331267eb6132513201ec39813ca292103
fernando ramirez <patricio_129@hotmail.com>
2023-04-02 14:14:20 -0600

    iniciando paginas redes sociales

---
+++ app/controllers/redessociales_controller.rb
@@ -0,0 +1,7 @@
+class RedessocialesController < ApplicationController
+  skip_before_action :authenticate_user!
+  def index
+
+  end
+
+end
---
+++ test/controllers/redessociales_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class RedessocialesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
